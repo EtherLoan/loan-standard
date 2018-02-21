4ecfa1f4fae626ba77d4ad6cbf15f871466ce953
Anibal Eduardo <anibal.catalan@consensys.net>
2018-02-20 21:21:34 -0500

    following guide style of solidity docs

--- contracts/standard/Loan.sol
+++ contracts/standard/Loan.sol
@@ -4,48 +4,48 @@ pragma solidity 0.4.18;
 /// @author anibal.catalan@consensys.net andres.junge@consensys.net
 contract Loan {
 
-  /// @notice funding the loan. (i.e. investors Rick send _capital Galactic Federation tokens to the contract, the contract transfers principal raised to borrower Morty, contract records Rick's ownership prorated by the amount each contributed).
-  /// @dev needs the token ERC20 functions to use _capital. This function will trigger Contribute event.
-  /// @param _capital is the amount of token to contribute.
-  function fund(uint256 _capital) public returns (bool success);
+    /// @notice funding the loan. (i.e. investors Rick send _capital Galactic Federation tokens to the contract, the contract transfers principal raised to borrower Morty, contract records Rick's ownership prorated by the amount each contributed).
+    /// @dev needs the token ERC20 functions to use _capital. This function will trigger Contribute event.
+    /// @param _capital is the amount of token to contribute.
+    function fund(uint256 _capital) public returns (bool success);
 
-  /// @notice retire capital from the loan. (i.e. borrower Morty has already paid back his due and investor Rick withdraw his loot).
-  /// @dev Needs the token ERC20 functions to transfer _capital to msg.sender. This function will trigger Withdraw event.
-  /// @param _capital is the amount of token to retire.
-  function withdraw(uint256 _capital) public returns (bool success);
+    /// @notice retire capital from the loan. (i.e. borrower Morty has already paid back his due and investor Rick withdraw his loot).
+    /// @dev Needs the token ERC20 functions to transfer _capital to msg.sender. This function will trigger Withdraw event.
+    /// @param _capital is the amount of token to retire.
+    function withdraw(uint256 _capital) public returns (bool success);
 
-  /// @notice the borrower accept loan termns and collect the capital. (i.e. the principal raised at the moment is X tokens and borrower Morty needs Y<X tokens, then Morty collect the principal raised and accept the loan terms).
-  /// @dev change the loan stage to repayment.
-  function accept() public returns (bool success);
+    /// @notice the borrower accept loan termns and collect the capital. (i.e. the principal raised at the moment is X tokens and borrower Morty needs Y<X tokens, then Morty collect the principal raised and accept the loan terms).
+    /// @dev change the loan stage to repayment.
+    function accept() public returns (bool success);
 
-  /// @notice pay back the due by borrower. (i.e. Morty send _payment Galactic Federation tokens to the contract, the contract distributes the _paymen to loan investors prorated by the amount each contributed).
-  /// @dev Needs the token ERC20 functions to transfer _payment. This function will trigger PayBack event.
-  /// @param _payment is the amount of token to payback.
-  function payback(uint256 _payment) public returns (bool success);
+    /// @notice pay back the due by borrower. (i.e. Morty send _payment Galactic Federation tokens to the contract, the contract distributes the _paymen to loan investors prorated by the amount each contributed).
+    /// @dev Needs the token ERC20 functions to transfer _payment. This function will trigger PayBack event.
+    /// @param _payment is the amount of token to payback.
+    function payback(uint256 _payment) public returns (bool success);
 
-  /// @notice return the loan current stage. (i.e. Summer wants contribute in Morty loan, but first need see if the loan is in funding stage).
-  /// @dev return loan current stage.
-  /// @return current stage
-  function stage() public view returns (uint8);
+    /// @notice return the loan current stage. (i.e. Summer wants contribute in Morty loan, but first need see if the loan is in funding stage).
+    /// @dev return loan current stage.
+    /// @return current stage
+    function stage() public view returns (uint8);
 
-  /// @notice trigger when contribute function is successfully called.
-  /// @dev trigger when contribute function is successfully called.
-  /// @param capital amount of token to contribute.
-  event Fund(uint256 indexed capital);
+    /// @notice trigger when contribute function is successfully called.
+    /// @dev trigger when contribute function is successfully called.
+    /// @param capital amount of token to contribute.
+    event Fund(uint256 indexed capital);
 
-  /// @notice trigger when withdraw function is successfully called.
-  /// @dev trigger when withdraw function is successfully called.
-  /// @param capital amount of token to retire.
-  event Withdraw(uint256 indexed capital);
+    /// @notice trigger when withdraw function is successfully called.
+    /// @dev trigger when withdraw function is successfully called.
+    /// @param capital amount of token to retire.
+    event Withdraw(uint256 indexed capital);
 
-  /// @notice trigger when collect function is successfully called.
-  /// @dev trigger when collect function is successfully called.
-  /// @param principal total amount lended.
-  event Accept(uint256 indexed principal);
+    /// @notice trigger when collect function is successfully called.
+    /// @dev trigger when collect function is successfully called.
+    /// @param principal total amount lended.
+    event Accept(uint256 indexed principal);
 
-  /// @notice trigger when payback function is successfully called.
-  /// @dev trigger when payback function is successfully called.
-  /// @param payment amount of token to repay.
-  event PayBack(uint256 indexed payment);
+    /// @notice trigger when payback function is successfully called.
+    /// @dev trigger when payback function is successfully called.
+    /// @param payment amount of token to repay.
+    event PayBack(uint256 indexed payment);
 
 }
